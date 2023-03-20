f401c5cb1d615a65aeee515afa44e137fe2475de
efrainamezcua <43830451+efrainamezcua@users.noreply.github.com>
2023-03-19 20:36:03 -0700

    Update 0231: Power of Two.cpp

--- 0231: Power of Two.cpp
+++ 0231: Power of Two.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         bool seen = false;
-        if (n&1) return n == 1 ? true: false; n >>= 1;
         while (n) {
             if (seen && n&1) return false;
             if (n&1) seen = true;
